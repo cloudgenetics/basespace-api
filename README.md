8fb04a0617575691a35c79fd7657d5bc34fab834
Krishna Kumar <krishnak@utexas.edu>
2021-10-23 20:14:26 -0500

    :construction: Download data from BaseSpace projects

---
+++ README.md
@@ -0,0 +1,11 @@
+# Fetch BaseSpace files and upload to S3
+> Krishna Kumar
+
+## Illumina BaseSpace AccessToken
+
+1. Go to https://developer.basespace.illumina.com/ and log in.
+2. From the toolbar, select My Apps.
+3. In the applications tab, select Create a New Application.
+4. Fill out the Applications Details and then select Create Application.
+5. On the application page, select the Credentials tab and copy the Access Token.
+
---
+++ fetch.py
@@ -0,0 +1,121 @@
+import urllib.request
+import requests
+import json
+import math
+import sys
+import os
+import socket
+import optparse
+
+
+def arg_parser():
+    cwd_dir = os.getcwd()
+    parser = optparse.OptionParser()
+    parser.add_option('-p', dest='projectid', help='Project ID: required')
+    parser.add_option('-a', dest='accesstoken', help='Access Token: required')
+    (options, args) = parser.parse_args()
+
+    try:
+        if options.projectid == None:
+            raise Exception
+        if options.accesstoken == None:
+            raise Exception
+    except Exception:
+        print("Usage: fetch.py -p <projectid> -a <AccessToken>")
+        sys.exit()
+
+    return options
+
+
+def restrequest(url):
+    r = requests.get(url)
+    return r.json()
+
+
+def downloadrestrequest(rawrequest, path):
+    dirname = projectid + os.sep + os.path.dirname(path)
+    # print(dirname)
+
+    if dirname != '':
+        if not os.path.isdir(dirname):
+            os.makedirs(dirname)
+
+    request = (rawrequest)
+
+    outfile = open(projectid + os.sep + path, 'wb')
+
+    try:
+        response = urllib.request.urlopen(request, timeout=1)
+        outfile.write(response.read())
+        outfile.close()
+
+    except urllib.request.URLError:
+        print('Got an error code:', urllib.request.URLError)
+        outfile.close()
+        downloadrestrequest(rawrequest, path)
+
+    except socket.error:
+        print('Got a socket error: retrying')
+        outfile.close()
+        downloadrestrequest(rawrequest, path)
+
+
+options = arg_parser()
+
+projectid = options.projectid
+AccessToken = options.accesstoken
+
+hreflist = []
+hrefcontentlist = []
+pathlist = []
+samplelist = []
+
+# Step 1: Find the Biosample ID from project id first
+# assume fewer than 1000 samples in a project
+url = 'https://api.basespace.illumina.com/v2/biosamples?projectid=%s&access_token=%s&limit=1000' % (
+    projectid, AccessToken)
+json_obj = restrequest(url)
+nSamples = len(json_obj['Items'])
+
+for sampleindex in range(nSamples):
+    sampleid = json_obj['Items'][sampleindex]['Id']
+    samplelist.append(sampleid)
+
+samplecsv = ','.join([str(i) for i in samplelist])
+print(samplecsv)
+
+# Step 2: Call API to get datasets based on biosample
+url = 'https://api.basespace.illumina.com/v2/datasets?inputbiosamples=%s&access_token=%s' % (
+    samplecsv, AccessToken)
+
+json_obj = restrequest(url)
+totalCount = int(json_obj['Paging']['TotalCount'])
+noffsets = int(math.ceil(float(totalCount)/1000.0))
+
+for index in range(noffsets):
+    offset = 1000*index
+    url = 'https://api.basespace.illumina.com/v2/datasets?inputbiosamples=%s&access_token=%s&limit=1000&Offset=%s' % (
+        samplecsv, AccessToken, offset)
+    json_obj = restrequest(url)
+    nDatasets = len(json_obj['Items'])
+    for fileindex in range(nDatasets):
+        href = json_obj['Items'][fileindex]['HrefFiles']
+        hreflist.append(href)
+
+# Step 3: Get the download filepath (HrefContent) and filename (Path)
+# normally two files per dataset in our case
+for index in range(len(hreflist)):
+    url = '%s?access_token=%s' % (hreflist[index], AccessToken)
+    json_obj = restrequest(url)
+    nfiles = len(json_obj['Items'])
+    for fileindex in range(nfiles):
+        hrefcontent = json_obj['Items'][fileindex]['HrefContent']
+        hrefcontentlist.append(hrefcontent)
+        path = json_obj['Items'][fileindex]['Path']
+        pathlist.append(path)
+
+for index in range(len(hreflist)):
+    url = '%s?access_token=%s' % (hrefcontentlist[index], AccessToken)
+    print(url)
+    print('downloading %s' % (pathlist[index]))
+    downloadrestrequest(url, pathlist[index])
