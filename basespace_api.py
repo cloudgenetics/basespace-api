ad48f4c0902848c1b7df6d50b56bb4148540771d
Krishna Kumar <krishnak@utexas.edu>
2021-10-24 12:41:39 -0500

    :construction: Refactor BaseSpace API fetch dataset

--- basespace_api.py
+++ basespace_api.py
@@ -13,74 +13,106 @@ class BaseSpaceAPI():
         self.baseurl = url
         self.session = HTTPSession()
 
-    def download_rest_request(self, url, path):
-        """Create a download folder and download file
+    def project_biosample_ids(self, maxsamples=1000):
+        """Fetch BioSample IDs from a Project
+
+        Args:
+            limit (int): Maximum number of biosamples
+        """
+        # Find the Biosample ID from project id first  assume fewer than 1000 default max samples in a project
+        response = self.session.get_json(self.baseurl + 'biosamples?projectid=%s&access_token=%s&limit=%d' % (
+            self.project_id, self.access_token, maxsamples))
+
+        biosamples = []
+        for sample in response['Items']:
+            biosamples.append(sample['Id'])
+
+        return biosamples
+
+    def project_mkdir(self, path):
+        """Make directory for a project
+
+        Args:
+            path (str): Path to create directory
         """
         dirname = self.project_id + os.sep + os.path.dirname(path)
-        # print(dirname)
+        status = False
         if dirname != '':
             if not os.path.isdir(dirname):
                 os.makedirs(dirname)
+                status = True
+        return status
+
+    def download_dataset(self, url, path):
+        """Download sample dataset
+
+        Args:
+            url (str): URL from which to download dataset
+            path (str): Download path
+        """
+        self.project_mkdir(path)
         outfile = self.project_id + os.sep + path
-        print("URL {} outfile {}".format(url, outfile))
         self.session.download_file(url, outfile)
 
     def download_projects(self):
         """Download project files
         """
-        hreflist = []
-        hrefcontentlist = []
-        pathlist = []
-        samplelist = []
 
-        # Step 1: Find the Biosample ID from project id first
-        # assume fewer than 1000 samples in a project
-        resp_json = self.session.get_json(self.baseurl + 'biosamples?projectid=%s&access_token=%s&limit=1000' % (
-            self.project_id, self.access_token))
-        nSamples = len(resp_json['Items'])
+        # Find the Biosample ID from project id first  assume fewer than 1000 samples in a project
+        biosamples = self.project_biosample_ids(maxsamples=1000)
 
-        for sampleindex in range(nSamples):
-            sampleid = resp_json['Items'][sampleindex]['Id']
-            samplelist.append(sampleid)
+        samples_csv = ','.join([str(i) for i in biosamples])
+        print("Samples {}".format(samples_csv))
 
-        samplecsv = ','.join([str(i) for i in samplelist])
-        print("Samples {}".format(samplecsv))
-
-        # Step 2: Call API to get datasets based on biosample
+        # Call API to get datasets based on biosamples
         url = self.baseurl + \
             'datasets?inputbiosamples=%s&access_token=%s' % (
-                samplecsv, self.access_token)
+                samples_csv, self.access_token)
 
-        json_obj = self.session.get_json(url)
-        totalCount = int(json_obj['Paging']['TotalCount'])
-        noffsets = int(math.ceil(float(totalCount)/1000.0))
+        response = self.session.get_json(url)
+        total_count = int(response['Paging']['TotalCount'])
+        noffsets = int(math.ceil(float(total_count)/1000.0))
+
+        hrefs = []
+        hrefcontents = []
+        paths = []
 
         for index in range(noffsets):
             offset = 1000*index
             url = self.baseurl + 'datasets?inputbiosamples=%s&access_token=%s&limit=1000&Offset=%s' % (
-                samplecsv, self.access_token, offset)
-            json_obj = self.session.get_json(url)
-            nDatasets = len(json_obj['Items'])
+                samples_csv, self.access_token, offset)
+            response = self.session.get_json(url)
+            nDatasets = len(response['Items'])
             for fileindex in range(nDatasets):
-                href = json_obj['Items'][fileindex]['HrefFiles']
-                hreflist.append(href)
+                href = response['Items'][fileindex]['HrefFiles']
+                hrefs.append(href)
 
-        # Step 3: Get the download filepath (HrefContent) and filename (Path)
-        # normally two files per dataset in our case
-        for index in range(len(hreflist)):
-            url = '%s?access_token=%s' % (hreflist[index], self.access_token)
-            print("Step3 URL: {}".format(url))
-            json_obj = self.session.get_json(url)
-            nfiles = len(json_obj['Items'])
+        # Get the download filepath (HrefContent) and filename (Path)
+        files = []
+        for href in hrefs:
+            url = '%s?access_token=%s' % (href, self.access_token)
+            response = self.session.get_json(url)
+            for dataset in response['Items']:
+                hrefcontent = dataset['HrefContent']
+                path = dataset['Path']
+                files.append((hrefcontent, path))
+
+        print("Files: {} ".format(files))
+
+        for index in range(len(hrefs)):
+            url = '%s?access_token=%s' % (hrefs[index], self.access_token)
+            response = self.session.get_json(url)
+            nfiles = len(response['Items'])
             for fileindex in range(nfiles):
-                hrefcontent = json_obj['Items'][fileindex]['HrefContent']
-                hrefcontentlist.append(hrefcontent)
-                path = json_obj['Items'][fileindex]['Path']
-                pathlist.append(path)
+                hrefcontent = response['Items'][fileindex]['HrefContent']
+                hrefcontents.append(hrefcontent)
+                path = response['Items'][fileindex]['Path']
+                paths.append(path)
+                print("File: {} href {} path {}".format(
+                    fileindex, hrefcontent, path))
 
-        for index in range(len(hreflist)):
+        for index in range(len(hrefcontents)):
             url = '%s?access_token=%s' % (
-                hrefcontentlist[index], self.access_token)
-            print(url)
-            print('downloading %s' % (pathlist[index]))
-            self.download_rest_request(url, pathlist[index])
+                hrefcontents[index], self.access_token)
+            print('downloading %s' % (paths[index]))
+            self.download_dataset(url, paths[index])
