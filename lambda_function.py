a71aa6aeaa29dc8ce1d715bcb70ec8252665e4f4
Krishna Kumar <krishnak@utexas.edu>
2022-02-05 14:21:27 -0600

    Accept APIURL as an optional argument

--- lambda_function.py
+++ lambda_function.py
@@ -4,15 +4,21 @@ from basespace_api import BaseSpaceAPI
 
 def lambda_handler(event, context):
     # Get access token and project id
+    
     data = event
     access_token = data['accessToken']
     projectid = data['projectId']
     uuid = data['uuid']
+    apiurl = 'https://api.euc1.sh.basespace.illumina.com/v2/'
+    if 'apiurl' in data:
+        apiurl = data['url']
+        
     # Create a BaseSpace instance
-    bs = BaseSpaceAPI(projectid, access_token)
+    bs = BaseSpaceAPI(projectid, access_token, apiurl)
     # Upload to S3
     files = bs.upload_basespace_project_to_s3(uuid)
+    
     return {
         'statusCode': 200,
-        'body': json.dumps(files)
+        'files': json.dumps(files)
     }
